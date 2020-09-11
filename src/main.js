e3b874fe7c402df0339c2c9575e2f05ddd4afed6
Alapher W. Hearty <alapherwori8g5@gmail.com>
2020-09-11 22:55:08 +0100

    Fixed Code

--- src/main.js
+++ src/main.js
@@ -3,14 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
-import js from './js/app'
 
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  js,
   store,
   vuetify,
   render: h => h(App)
